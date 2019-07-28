63bfb98874865cf92acf6ac73d327b9843a259b6
keisukeadachi <c3a578zx@gmail.com>
2019-07-28 15:36:25 +0900

    devise_sample_0728

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Item.create(name: "cherry本", price: 2200, user_id: 1)
+Item.create(name: "速習実践ガイド", price: 2100, user_id: 1)
+Item.create(name: "Webを支える技術", price: 3000, user_id: 1)
